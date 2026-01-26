10dabb999aea7c1ebbb5375142a69f3f0b2a6276
Chloe North <cn297498@falmouth.ac.uk>
2026-01-26 23:31:30 +0000

    finished the countries and edited prefab

--- Assets/Scripts/ProvinceStats.cs
+++ Assets/Scripts/ProvinceStats.cs
@@ -8,16 +8,87 @@ using TMPro;
 public class ProvinceStats : MonoBehaviour
 {
     public Province provinceData;
-    public string provinceName;
     public bool hasArmy, hasFleet;
     public int controllingPower;
+    public Color iconColour;
+    public Image army, fleet, supply;
+    public TextMeshProUGUI nameText;
     void Start()
     {
+        switch (provinceData.startingPower)
+        {
+            case 0:
+                iconColour = Color.white;
+                break;
+            case 1:
+                iconColour = Color.blue;
+                break;
+            case 2:
+                iconColour = Color.cyan;
+                break;
+            case 3:
+                iconColour = Color.black;
+                break;
+            case 4:
+                iconColour = Color.green;
+                break;
+            case 5:
+                iconColour = Color.red;
+                break;
+            case 6:
+                iconColour = Color.magenta;
+                break;
+            case 7:
+                iconColour = Color.yellow;
+                break;
+        }
         UpdateProvinceStats();
     }
 
     public void UpdateProvinceStats()
     {
-        provinceName = provinceData.provinceName;
+        nameText.text = provinceData.provinceName;
+        switch (controllingPower)
+        {
+            case 0:
+                iconColour = Color.white;
+                break;
+            case 1:
+                iconColour = Color.blue;
+                break;
+            case 2:
+                iconColour = Color.cyan;
+                break;
+            case 3:
+                iconColour = Color.black;
+                break;
+            case 4:
+                iconColour = Color.green;
+                break;
+            case 5:
+                iconColour = Color.red;
+                break;
+            case 6:
+                iconColour = Color.magenta;
+                break;
+            case 7:
+                iconColour = Color.yellow;
+                break;
+        }
+        army.color = iconColour;
+        fleet.color = iconColour;
+        supply.color = iconColour;
+        if (provinceData.isSupply)
+        {
+            supply.gameObject.SetActive(true);
+        }
+        if (hasArmy)
+        {
+            army.gameObject.SetActive(true);
+        }
+        if (hasFleet)
+        {
+            fleet.gameObject.SetActive(true);
+        }
     }
 }
