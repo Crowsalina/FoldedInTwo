ae76e3f848c8d4c1b9a5350e69e14f814e72eae4
Chloe North <cn297498@falmouth.ac.uk>
2026-01-28 01:38:32 +0000

    added full spawning and spawners

--- Assets/Scripts/ProvinceSpawningManager.cs
+++ Assets/Scripts/ProvinceSpawningManager.cs
@@ -1,5 +1,6 @@
 using Crowsalina;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ProvinceSpawningManager : MonoBehaviour
@@ -9,15 +10,18 @@ public class ProvinceSpawningManager : MonoBehaviour
     private GameObject provincePrefab;
     [SerializeField]
     private Transform ProvinceHandler;
-    [SerializeField]
     List<GameObject> spawnLocations = new List<GameObject>();
+
     private void Start()
     {
-        Province[] provinces = Resources.LoadAll<Province>("Provinces");
+        Province[] provinces = Resources.LoadAll<Province>("Provinces").OrderBy(go => go.provinceName).ToArray();
         allProvinces.AddRange(provinces);
+        GameObject[] spawns = GameObject.FindGameObjectsWithTag("SpawnLocation").OrderBy(go => go.name).ToArray();
+        spawnLocations.AddRange(spawns);
     }
     public void SpawnAllProvinces()
     {
+        Debug.Log(allProvinces.Count.ToString() + " - " + spawnLocations.Count.ToString());
         for (int i = 0; i < allProvinces.Count; i++)
         {
             SpawnProvince(allProvinces[i], spawnLocations[i]);
--- Assets/Scripts/ProvinceStats.cs
+++ Assets/Scripts/ProvinceStats.cs
@@ -10,71 +10,61 @@ public class ProvinceStats : MonoBehaviour
     public Province provinceData;
     public bool hasArmy, hasFleet;
     public int controllingPower;
-    public Color iconColour;
-    public Image army, fleet, supply;
+    public Color iconColour, buttonColour;
+    public Image army, fleet, supply, buttonImage;
     public TextMeshProUGUI nameText;
     void Start()
     {
-        switch (provinceData.startingPower)
-        {
-            case 0:
-                iconColour = Color.white;
-                break;
-            case 1:
-                iconColour = Color.blue;
-                break;
-            case 2:
-                iconColour = Color.cyan;
-                break;
-            case 3:
-                iconColour = Color.black;
-                break;
-            case 4:
-                iconColour = Color.green;
-                break;
-            case 5:
-                iconColour = Color.red;
-                break;
-            case 6:
-                iconColour = Color.magenta;
-                break;
-            case 7:
-                iconColour = Color.yellow;
-                break;
-        }
+        controllingPower = provinceData.startingPower;
         UpdateProvinceStats();
     }
-
     public void UpdateProvinceStats()
     {
         nameText.text = provinceData.provinceName;
         switch (controllingPower)
         {
             case 0:
-                iconColour = Color.white;
+                if (provinceData.isMaritime)
+                {
+                    buttonColour = new Color(0.803f, 0.941f, 0.962f, 1);
+                }
+                else
+                {
+                    iconColour = Color.white;
+                    buttonColour = new Color(0.76f, 0.76f, 0.76f, 1);
+                }
                 break;
             case 1:
                 iconColour = Color.blue;
+                buttonColour = new Color(0.463f, 0.435f, 0.698f, 1);
                 break;
             case 2:
                 iconColour = Color.cyan;
+                buttonColour = new Color(0.549f, 0.780f, 0.925f, 1);
                 break;
             case 3:
                 iconColour = Color.black;
+                buttonColour = new Color(0.43f, 0.43f, 0.43f, 1);
                 break;
             case 4:
                 iconColour = Color.green;
+                buttonColour = new Color(0.42f, 0.573f, 0.404f, 1);
                 break;
             case 5:
                 iconColour = Color.red;
+                buttonColour = new Color(0.678f, 0.295f, 0.223f, 1);
                 break;
             case 6:
                 iconColour = Color.magenta;
+                buttonColour = new Color(0.678f, 0.223f, 0.598f, 1);
                 break;
             case 7:
                 iconColour = Color.yellow;
+                buttonColour = new Color(1, 0.969f, 0.518f, 1);
                 break;
         }
+
+        buttonImage.color = buttonColour;
         army.color = iconColour;
         fleet.color = iconColour;
         supply.color = iconColour;
